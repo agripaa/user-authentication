033185db7137c8e82e1ef1f10572550c9739350c
Jounnyy <agrieva.xananda08@gmail.com>
2022-04-28 03:53:48 +0700

    caffeew

---
+++ src/components/pages/FullProduct.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FullProduct = () => {
+  return (
+    <section id='full-product'>
+      <div classsName='container-fluid'>
+        <div classsName='row'>
+          Hello
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default FullProduct
