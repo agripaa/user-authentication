7fdb9149a0f67b3df178302607c03d9014b88b79
Jounnyy <agrieva.xananda08@gmail.com>
2022-05-08 23:46:40 +0700

    fixed button about

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
                 <div className='col-md-8 mt-5 content'>
                     <h1 className='display-4 fw-bolder mb-4 text-center'>Wellcome To Coffee Shop!!</h1>
                     <p className='lead text-center fs-4 mb-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Suspendisse eget imperdiet lorem, quis bibendum velit. Nulla suscipit leo vel semper ornare. Mauris a dui ultrices, euismod metus interdum, pulvinar turpis.</p>
-                    <div className='buttons d-flex justify-content-center'>
+                    <div className='butons d-flex justify-content-center'>
                         <button className='btn buttons me-4 rounded-pill px-4 py-2'>Get Qoute</button>
                         <button className='btn buttonss me-4 rounded-pill px-4 py-2'>Our Service</button>
                     </div>
